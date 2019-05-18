1f8b7f52ee552225b978888dede56a7e1267be23
Nick Playfair <nick@chaosemerald.co.uk>
2019-05-18 10:22:07 +0100

    Add events and get question function

--- js/main.js
+++ js/main.js
@@ -22,3 +22,30 @@ const inputField = document.querySelector('#input-field');
 const inputLabel = document.querySelector('#input-label');
 const inputProgress = document.querySelector('#input-progress');
 const progress = document.querySelector('#progress-bar');
+
+// Events
+document.addEventListener('DOMContentLoaded', getQuestion);
+
+
+
+// Functions
+
+// Get question from array and add to markup
+function getQuestion() {
+    // Get current question
+    inputLabel.innerHTML = questions[position].question;
+    // Get current type
+    inputField.type = questions[position].type || 'text';
+    // Get current answer
+    inputField.value = questions[position].answer || '';
+    // Focus on element
+    inputField.focus();
+
+    // Set progress bar width - variable to the questions length
+    progress.getElementsByClassName.width = (position * 100 ) / questions.length + '%';
+
+    // Add user icon or back arrow, depending on question
+    prevBtn.className = position ? 'fas fa-arrow-left' : 'fas fa-user';
+
+    showQuestion();
+}
