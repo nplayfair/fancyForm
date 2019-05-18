ff4da7865b631083f1da7da7bae21d6a0881be59
Nick Playfair <nick@chaosemerald.co.uk>
2019-05-18 10:52:19 +0100

    Allow enter key to complete input field

--- js/main.js
+++ js/main.js
@@ -31,6 +31,13 @@ document.addEventListener('DOMContentLoaded', getQuestion);
 // Next button click
 nextBtn.addEventListener('click', validate);
 
+// Input field enter press
+inputField.addEventListener('keyup', e => {
+    if (e.keyCode === 13) {
+        validate();
+    }
+});
+
 
 // Functions
 
